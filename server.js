83538bfd98805c265bc8065205d4f0e3c5b5d7ce
kaiserofthenight <abderrazzak.elouazghi@gmail.com>
2025-07-11 19:56:11 +0200

    monster2

--- server.js
+++ server.js
@@ -12,12 +12,23 @@ dotenv.config();
 const app = express();
 
 // Middleware
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://foundup.vercel.app',
+  'https://frontend-foudup.vercel.app'
+];
+
 app.use(cors({
-  origin: 'http://localhost:5173', 
-  credentials: true,              
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
 }));
+
 
 
 app.use(express.json());
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -8,7 +8,7 @@ export const generateToken = (userId, res) => {
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
         sameSite: "lax",
-        secure: false //process.env.NODE_ENV !== "development"
+        secure: process.env.NODE_ENV !== "development"
     });
     return token;
 };
