2cbddc5b6111c7e280469c07a61938c145eec460
kaiserofthenight <abderrazzak.elouazghi@gmail.com>
2025-07-11 21:02:00 +0200

    monster4

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -7,8 +7,9 @@ export const generateToken = (userId, res) => {
     res.cookie("jwt", token, {
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        sameSite: "lax",
-        secure: process.env.NODE_ENV !== "development"
+        sameSite: "none", 
+        secure: true, 
+        domain: process.env.NODE_ENV === "production" ? ".vercel.app" : undefined
     });
     return token;
 };
