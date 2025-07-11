f0ae3683a6c4b898f22ed5c9a46b1a4708b79e73
kaiserofthenight <abderrazzak.elouazghi@gmail.com>
2025-07-11 19:21:55 +0200

    fix

--- server.js
+++ server.js
@@ -13,10 +13,12 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: '*', 
+  origin: 'http://localhost:5173', 
+  credentials: true,              
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
+
 
 app.use(express.json());
 app.use(cookieParser());
