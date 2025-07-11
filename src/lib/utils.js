4f6883702c8668ae279665759123435fb0d8d70b
kaiserofthenight <abderrazzak.elouazghi@gmail.com>
2025-07-11 19:46:24 +0200

    you

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -8,7 +8,7 @@ export const generateToken = (userId, res) => {
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
         sameSite: "lax",
-        secure: process.env.NODE_ENV !== "development"
+        secure: false //process.env.NODE_ENV !== "development"
     });
     return token;
 };
